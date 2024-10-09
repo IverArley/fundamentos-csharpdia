658f1dd0332659352f448a3926eb7c31cc8df5aa
Iver Mendez <iverarleyb@gmail.com>
2024-10-09 16:33:34 -0500

    Se crea la clase cary el formulario

---
+++ EjemploFundamentosCSharp/Proyecto2/Index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto2
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //Invocar
+            mostarMensaje();
+        }
+        //Metodo para mostrar el mensaje por el navegador
+        public void mostarMensaje()
+        {
+            Car car = new Car();
+            string msj = car.stop();
+
+            LblMensaje.Text = msj;
+        }
+    }
+}
